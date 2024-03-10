f8fa8e6c0c8192139be438d4aad8037d0d873e7f
Kawson <piegusek20@gmail.com>
2024-03-10 19:19:16 +0100

    modified:   imageInfo.py

--- imageInfo.py
+++ imageInfo.py
@@ -1,10 +1,15 @@
-import PIL.Image
+from PIL import Image, ExitTags
+import PIL
 
 class ImageInfo:
     @staticmethod
     def get_image_info(image_path):
         img = PIL.Image.open(image_path)
         info = img.getexif()
-        print(info)
+        if info is None:
+            print('Sorry, image has no exif data.')
+        else:
+            for key, val in info.items():
+                print(f'{key}:{val}')
         
 ImageInfo.get_image_info("d:\PG\SEM 8\Train\Healthy\8bc2979962db6549.jpg")
