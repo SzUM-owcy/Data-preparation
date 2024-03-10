662e7a7f371291571107ee9f7721e089df224eac
Kawson <piegusek20@gmail.com>
2024-03-10 18:57:54 +0100

    update function

--- JoinDatasets.py
+++ JoinDatasets.py
@@ -1,23 +1,20 @@
 import shutil
 import os
 
-main_dir = input()
-output = input()
-
-
 labels = ["Healthy","Powdery","Rust"]
 sets = ["Test","Train","Validation"]
-for label in labels:
-    for set in sets:
-        dir_path = os.path.join(main_dir,set,set,label)    
-        files = os.listdir(dir_path)
-        for file in files:
-            src = os.path.join(dir_path,file)
-            dst = os.path.join(output,label)
-            if not os.path.exists(dst):
-                os.mkdir(dst)
-            
-            
-            shutil.copy2(src,dst)
-            
-        pass
+
+def join_data_sets(main_dir, output_dir):
+    for label in labels:
+        for set in sets:
+            dir_path = os.path.join(main_dir,set,set,label)    
+            files = os.listdir(dir_path)
+            for file in files:
+                src = os.path.join(dir_path,file)
+                dst = os.path.join(output_dir,label)
+                if not os.path.exists(dst):
+                    os.mkdir(dst)
+                
+                shutil.copy2(src,dst)
+                
+            pass
