8f1ce331c8e31861b672f33e37d3e5a8374f5ff7
Kawson <piegusek20@gmail.com>
2024-03-10 19:33:59 +0100

    add docstrings

--- imageInfo.py
+++ imageInfo.py
@@ -1,8 +1,17 @@
 from PIL import Image
 
 class ImageInfo:
+    
     @staticmethod
     def get_image_size(image_path):
+        """
+        Get the width and height of the image.
+
+        :param image_path: Path to the image file.
+        :type image_path: str
+        :return: Width and height of the image.
+        :rtype: tuple(int, int)
+        """
         try:
             img = Image.open(image_path)
             width, height = img.size
@@ -13,6 +22,14 @@ class ImageInfo:
             
     @staticmethod
     def get_image_rgb_matrix(image_path):
+        """
+        Get the RGB color matrix of the image.
+
+        :param image_path: Path to the image file.
+        :type image_path: str
+        :return: RGB color matrix of the image.
+        :rtype: list(tuple(int, int, int))
+        """
         try:
             img = Image.open(image_path)
             rgb_matrix = list(img.getdata())
