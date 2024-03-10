41ed517ab547115d6cdf302a46428d8d10dd635a
Krzysztof Nazar <s184698@student.pg.edu.pl>
2024-03-10 19:08:55 +0100

    merge data

--- JoinDatasets.py
+++ JoinDatasets.py
@@ -1,20 +1,21 @@
 import shutil
 import os
 
-labels = ["Healthy","Powdery","Rust"]
-sets = ["Test","Train","Validation"]
+labels = ["Healthy", "Powdery", "Rust"]
+sets = ["Test", "Train", "Validation"]
+
 
 def join_data_sets(main_dir, output_dir):
     for label in labels:
         for set in sets:
-            dir_path = os.path.join(main_dir,set,set,label)    
+            dir_path = os.path.join(main_dir, set, set, label)
             files = os.listdir(dir_path)
             for file in files:
-                src = os.path.join(dir_path,file)
-                dst = os.path.join(output_dir,label)
+                src = os.path.join(dir_path, file)
+                dst = os.path.join(output_dir, label)
                 if not os.path.exists(dst):
                     os.mkdir(dst)
-                
-                shutil.copy2(src,dst)
-                
+
+                shutil.copy2(src, dst)
+
             pass
