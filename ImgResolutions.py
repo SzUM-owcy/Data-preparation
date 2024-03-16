7c4aa3afbe36423ccde56aedd2ff44811953f8aa
rico <h.nacmer@gmail.com>
2024-03-16 23:05:51 +0100

    added resizing, cropping

--- ImgResolutions.py
+++ ImgResolutions.py
@@ -1,6 +1,7 @@
 import os
 from collections import Counter
 
+
 def res_image (filename):
     with open(filename, 'rb') as file_image:
             file_image.seek (163)
@@ -11,7 +12,7 @@ def res_image (filename):
             return (width,height)
             print("The resolution of the given image is:", width, "x", height)
 
-main_dir = "merged-data"
+main_dir = "merged-data\\unchanged"
 labels = ["Healthy","Powdery","Rust"]
 
 all_widths = []
@@ -32,10 +33,10 @@ for label in labels:
         
     print(label, " Mean Width:", sum(widths)/len(widths))
     print(label, "Mean Height:", sum(heights)/len(heights))
-    print(label, "Widths Count:",Counter(widths))
-    print(label, "Heights Count:",Counter(heights))
+    print(label, "Widths Count:", Counter(widths))
+    print(label, "Heights Count:", Counter(heights))
 
 print("Mean Width:", sum(all_widths)/len(all_widths))
 print("Mean Height:", sum(all_heights)/len(all_heights))
-print("Widths Count:",Counter(all_widths))
-print("Heights Count:",Counter(all_heights))
+print("Widths Count:", Counter(all_widths))
+print("Heights Count:", Counter(all_heights))
--- JoinDatasets.py
+++ JoinDatasets.py
@@ -1,21 +1,57 @@
 import shutil
 import os
+import multiprocessing
+import Normalisation
+import tqdm
+from skimage import io 
 
-labels = ["Healthy", "Powdery", "Rust"]
-sets = ["Test", "Train", "Validation"]
 
+def process_image(dir_path,file,output_dir,label,copy_version,image_shape):
+    src = os.path.join(dir_path, file)
+    dst = os.path.join(output_dir, label)
+    if not os.path.exists(dst):
+        os.makedirs(dst)
 
-def join_data_sets(main_dir, output_dir):
+    #Copy unchanged
+    if(copy_version == "unchanged"):
+        shutil.copy2(src, dst)
+        return
+
+    #Copy Resized
+    if(copy_version == "resize"):
+        img = Normalisation.Resize(src,image_shape[0],image_shape[1])
+    
+    #Copy Cropped
+    if(copy_version == "crop"):
+        img = Normalisation.CropFromMiddle(src,image_shape[0],image_shape[1]) 
+    #io.imshow(img)
+    io.imsave(os.path.join(dst,file), img, check_contrast=False)
+
+def join_datasets_with_shape_normalisation(main_dir, output_dir, copy_version, image_shape = (0,0)):
+    labels = ["Healthy", "Powdery", "Rust"]
+    sets = ["Test", "Train", "Validation"]
     for label in labels:
         for set in sets:
             dir_path = os.path.join(main_dir, set, set, label)
             files = os.listdir(dir_path)
-            for file in files:
-                src = os.path.join(dir_path, file)
-                dst = os.path.join(output_dir, label)
-                if not os.path.exists(dst):
-                    os.mkdir(dst)
+            parallel_input = [(dir_path,file,output_dir,label,copy_version,image_shape) for file in files]
+            
+            #Parallel
+            with multiprocessing.Pool(6) as pool:
+                if copy_version != "resized": 
+                    parallel_input = tqdm.tqdm(parallel_input, total = len(parallel_input))
+                pool.starmap(process_image, parallel_input, chunksize=2)
 
-                shutil.copy2(src, dst)
+            #Sequential
+            # for inp in parallel_input:
+            #     process_image(*inp)
+        
 
-            pass
+# if __name__ == '__main__':
+#     path = os.getcwd()
+#     join_datasets_with_shape_normalisation(os.path.join(path,"data"), os.path.join(path,"merged-data","unchanged"), "unchanged")
+#     print("Finished copying")
+#     join_datasets_with_shape_normalisation(os.path.join(path,"data"), os.path.join(path,"merged-data","cropped"), "crop", (2421,1728)) # 2592,1728
+#     print("Finished cropping")
+#     join_datasets_with_shape_normalisation(os.path.join(path,"data"), os.path.join(path,"merged-data","resized"), "resize", (3982,2700))
+#     print("Finished resizing")
