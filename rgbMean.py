65ed8a57e83dd6d42f7a1b4ec57c28eb4e4534f1
Kawson <piegusek20@gmail.com>
2024-03-17 22:40:25 +0100

    rgb values mean diagram

--- rgbMean.py
+++ rgbMean.py
@@ -1,15 +1,19 @@
 import imageInfo
+import os
+import multiprocessing
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 class RgbMean:
     """
     Get mean RGB value
 
-    :param image_path: Path to the image file.
-    :type image_path: str
+    :param rgb_matrix: RGB matrix of image.
+    :type rgb_matrix: 
     :return: RGB values mean.
     :rtype: tuple(int, int, int)
     """
-    def get_single_image_rgb_mean(rgb_matrix):
+    def get_rgb_matrix_mean(self, rgb_matrix):
         r_sum = 0
         g_sum = 0
         b_sum = 0
@@ -21,13 +25,36 @@ class RgbMean:
         g_avg = g_sum // len(rgb_matrix)
         b_avg = b_sum // len(rgb_matrix)
         return [r_avg, g_avg, b_avg]
-        
-        
-# import time
-# rgbMean = RgbMean
-# start = time.perf_counter()
-# # rgb_mean = rgbMean.get_single_image_rgb_mean(imageInfo.ImageInfo.get_image_rgb_matrix("D:\PG\SEM 8\Data-preparation\merged-data\Healthy\8bc2979962db6549.jpg"))
-# img = imageInfo.ImageInfo.get_image_rgb_matrix("D:\PG\SEM 8\Data-preparation\merged-data\Healthy\8bc2979962db6549.jpg")
-# end = time.perf_counter()
-# print(f"time = {end - start:0.4f}")
-# # print(rgb_mean[0])
+    
+    
+    def get_single_image_rgb_mean(self, file_path):
+        rgb_matrix = imageInfo.ImageInfo.get_image_rgb_matrix(file_path)
+        return self.get_rgb_matrix_mean(rgb_matrix)
+    
+    """
+    Get count of specific RGB colour
+
+    :param rgb_means: array that contains RGB means for images.
+    :type [[avg_R1, avg_G1, avg_B1], [avg_R2, avg_G2, avg_B2]...]: 
+    :return: count of specific RGB values.
+    :rtype: array[
+        [
+            [0:count],
+            [1:count],
+            [255:count]
+        ] # FOR Red,
+        [...] # FOR Green,
+        [...] # FOR Blue
+    ]
+    """
+    def get_rgb_count(self, rgb_means):
+        MIN_RGB_VALUE = 0
+        MAX_RGB_VALUE = 255
+        R_count = [0 for rgb_value in range(MIN_RGB_VALUE, MAX_RGB_VALUE + 1)]
+        G_count = [0 for rgb_value in range(MIN_RGB_VALUE, MAX_RGB_VALUE + 1)]
+        B_count = [0 for rgb_value in range(MIN_RGB_VALUE, MAX_RGB_VALUE + 1)]
+        for rgb_mean in rgb_means:
+            R_count[rgb_mean[0]] += 1
+            G_count[rgb_mean[1]] += 1
+            B_count[rgb_mean[2]] += 1
+        return [R_count, G_count, B_count]            
