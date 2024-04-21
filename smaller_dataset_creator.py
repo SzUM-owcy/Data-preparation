b757ba41943dc885a117ababb704b137fd074ef1
rico <h.nacmer@gmail.com>
2024-04-21 21:33:54 +0200

    some additions, changes and fixes

--- smaller_dataset_creator.py
+++ smaller_dataset_creator.py
@@ -12,19 +12,20 @@ def read_files(src_dir, new_dir):
         for set in sets:
             dir_path = src_dir + set + '/' + label
             files = os.listdir(dir_path)
+            os.makedirs(new_dir + set + '/' + label,exist_ok=True)
             for file in files:
                 src = dir_path + '/' + file
 
                 foo = Image.open(src)
 
                 # downsize the image with an ANTIALIAS filter (gives the highest quality)
-                foo = foo.resize((224, 224), Image.ANTIALIAS)
+                foo = foo.resize((225, 225), Image.LANCZOS)
 
                 new_filepath = new_dir + set + '/' + label + '/' + file
                 foo.save(new_filepath, optimize=True, quality=95)
 
 
-main_dir = 'splits/split1/'
-new_dir = 'splits/split1_new/'
+main_dir = 'splits/split3/'
+new_dir = 'splits/split3_new/'
 
 read_files(main_dir, new_dir)
